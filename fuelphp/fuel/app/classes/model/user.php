ced37ff568301d0cb12180a3ea1a18990f5ace1c
Le Toan <le.toan@mulodo.com>
2015-12-14 16:12:45 +0700

    Add UT for UserModel

---
+++ fuelphp/fuel/app/classes/model/user.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CRUD operations for User model
+ * @author letoan
+ *
+ */
+class Model_User extends \Fuel\Core\Model {
+    public function getData() {
+        return 'getData';
+    }
+
+    public function updateData() {
+         return 'updateData';
+    }
+
+    public function deleteData() {
+        return 'deleteData';
+    }
+
+    public function insertData() {
+        return 'insertData';
+    }
+}
--- fuelphp/fuel/app/tests/model/user.php
+++ fuelphp/fuel/app/tests/model/user.php
@@ -1,7 +1,34 @@
 <?php
 use Fuel\Core\TestCase;
 class Test_Model_User extends TestCase {
+    private $model_user;
+
+    public function setUp() {
+    	$this->model_user = new Model_User();
+    }
+
+    public function tearDown() {
+    	unset($this->model_user);
+    }
+
     public function test_a() {
         $this->assertTrue(true);
+    }
+
+    public function test_getDataUser() {
+        $this->assertEquals('getData', $this->model_user->getData());
+    }
+
+    public function test_updateDataUser() {
+        $this->assertEquals('updateData', $this->model_user->updateData());
+    }
+
+
+    public function test_insertDataUser() {
+        $this->assertEquals('insertData', $this->model_user->insertData());
+    }
+
+    public function test_deleteDataUser() {
+        $this->assertEquals('deleteData', $this->model_user->deleteData());
     }
 }
